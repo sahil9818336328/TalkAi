91d1364dff7c5b3b4fa297af61738d5b1f030d5b
sahil khatri <sahilkeshav1997@gmail.com>
2024-12-08 11:02:50 +0530

    clerk authentication

--- app/page.js
+++ app/page.js
@@ -1,4 +1,21 @@
+import Link from 'next/link'
 const HomePage = () => {
-  return <div>GPTGenius Starter Project</div>;
-};
-export default HomePage;
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-6xl font-bold text-primary'>TalkAi</h1>
+          <p className='py-6 text-lg leading-loose'>
+            TalkAi: Your AI language companion. Powered by OpenAI, it enhances
+            your conversations, content creation, and more!
+          </p>
+          <Link href='/chat' className='btn btn-secondary '>
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
