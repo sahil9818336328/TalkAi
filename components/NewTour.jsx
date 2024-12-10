f434e3916bbb55baff6b39f88e65636399276bd4
sahil khatri <sahilkeshav1997@gmail.com>
2024-12-10 10:18:05 +0530

    new tour page complete

---
+++ components/NewTour.jsx
@@ -0,0 +1,66 @@
+'use client'
+
+import toast from 'react-hot-toast'
+import TourInfo from '@/components/TourInfo'
+import { useMutation } from '@tanstack/react-query'
+import { generateTourResponse } from '@/utils/actions'
+
+const NewTour = () => {
+  const {
+    mutate,
+    isPending,
+    data: tour,
+  } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination)
+      if (newTour) {
+        return newTour
+      }
+      toast.error('No matching city found...')
+      return null
+    },
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const destination = Object.fromEntries(formData.entries())
+    mutate(destination)
+  }
+
+  if (isPending) {
+    return <span className='loading loading-lg'></span>
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className=' mb-4'>Select your dream destination</h2>
+        <div className='join w-full'>
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='city'
+            name='city'
+            required
+          />
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='country'
+            name='country'
+            required
+          />
+          <button className='btn btn-primary join-item' type='submit'>
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className='mt-16'>
+        <div className='mt-16'>{tour ? <TourInfo tour={tour} /> : null}</div>
+      </div>
+    </>
+  )
+}
+export default NewTour
